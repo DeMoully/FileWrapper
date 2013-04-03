eb48c66e56f3bdeeefdfe5f8bcc70639c0c120ea
Zach DeMoully <zachdemoully@hotmail.com>
2013-04-03 16:21:27 -0500

    Update to FileCloseAction Enum
    Changed the FileCloseAction enum to an enum class, which should help
    make the FileWrapper classes as compatible as possible with other
    libraries and projects.

--- FileCloseAction.hpp
+++ FileCloseAction.hpp
@@ -2,7 +2,7 @@
 
 namespace fileFunctions
 {
-	enum FileCloseAction
+	enum class FileCloseAction
 	{
 		NONE, // Perform no actions upon deletion of object
 		OUTPUT, // Output the current contents of the object to the file specified by fileName
