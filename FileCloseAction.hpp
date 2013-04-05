5c1463fe5d0ccde7c5264d132972afe3a34c6150
Zach DeMoully <zachdemoully@hotmail.com>
2013-04-05 18:40:35 -0500

    FileCloseAction Update
    Added stream insertion operator overload for FileCloseAction enum class

--- FileCloseAction.hpp
+++ FileCloseAction.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 namespace fileFunctions
 {
 	enum class FileCloseAction
@@ -8,4 +10,27 @@ namespace fileFunctions
 		OUTPUT, // Output the current contents of the object to the file specified by fileName
 		APPEND // Append the current contents of the object to the file specified by fileName
 	};
+
+	std::ostream & operator << (std::ostream & ostr, FileCloseAction rhs)
+	{
+		switch (rhs)
+		{
+		case FileCloseAction::NONE:
+			{
+				ostr << "NONE";
+				break;
+			}
+		case FileCloseAction::OUTPUT:
+			{
+				ostr << "OUTPUT";
+				break;
+			}
+		case FileCloseAction::APPEND:
+			{
+				ostr << "APPEND";
+				break;
+			}
+		}
+		return ostr;
+	}
 }
