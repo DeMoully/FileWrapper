028af186ef8f3b17952a9f329b8a3ba21f4b68ab
Zach DeMoully <zachdemoully@hotmail.com>
2013-04-28 18:14:50 -0500

    CommonFunctions Update
    Added two new functions, renameFile and removeFile, to the file. These
    may be used in the future.

--- CommonFunctions.hpp
+++ CommonFunctions.hpp
@@ -1,17 +1,32 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <cstdio>
 
 namespace fileFunctions
 {
 	namespace FWPF // FileWrapperPrivateFunctions
 	{
+		// If lowerBound >= upperBound, swap lowerBound and upperBound
 		void validateBounds(std::size_t & lowerBound, std::size_t & upperBound)
 		{
 			if (upperBound < lowerBound)
 			{
 				std::swap(lowerBound, upperBound);
 			}
+		}
+
+		// Rename the file 'oldName' to 'newName' if it exists
+		bool renameFile(const std::string & oldName, const std::string & newName)
+		{
+			return !std::rename(oldName.c_str(), newName.c_str());
+		}
+
+		// Removes the file 'fileName' if possible
+		bool removeFile(const std::string & fileName)
+		{
+			return !std::remove(fileName.c_str());
 		}
 	}
 }
