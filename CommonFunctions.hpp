2b75839c9b2aff1808d0b9053f912467ef701a04
Zach DeMoully <zachdemoully@hotmail.com>
2013-05-28 21:28:04 -0500

    Refactored Code

--- CommonFunctions.hpp
+++ CommonFunctions.hpp
@@ -8,24 +8,24 @@ namespace fileFunctions
 {
 	namespace FWPF // FileWrapperPrivateFunctions
 	{
-		// If lowerBound >= upperBound, swap lowerBound and upperBound
 		void validateBounds(std::size_t & lowerBound, std::size_t & upperBound)
 		{
+			// If lowerBound >= upperBound, swap lowerBound and upperBound
 			if (upperBound < lowerBound)
 			{
 				std::swap(lowerBound, upperBound);
 			}
 		}
 
-		// Rename the file 'oldName' to 'newName' if it exists
 		bool renameFile(const std::string & oldName, const std::string & newName)
 		{
+			// Rename the file 'oldName' to 'newName' if it exists
 			return !std::rename(oldName.c_str(), newName.c_str());
 		}
 
-		// Removes the file 'fileName' if possible
 		bool removeFile(const std::string & fileName)
 		{
+			// Removes the file 'fileName' if possible
 			return !std::remove(fileName.c_str());
 		}
 	}
